bbd323fcc932cc0fd20ba98fc7d9417d036baaa4
trk20 <trkennedy01@gmail.com>
2024-10-01 13:29:32 -0400

    Small refactor

--- UserReplay/Parse.cs
+++ UserReplay/Parse.cs
@@ -1,5 +1,8 @@
 
+using System.Globalization;
+using System.Net;
 using CommandLine;
+using Flurl.Http;
 using Newtonsoft.Json.Linq;
 using Serilog;
 
@@ -26,33 +29,11 @@ namespace UserReplay
                     Session session = new();
                     var entries = har["log"]["entries"];
                     var requests = new List<ParsedRequest>();
-                    foreach (var entry in entries)
+                    foreach (var entry in entries.Cast<JObject>())
                     {
-                        JObject parameters = new();
-                        foreach (var query in entry["request"]["queryString"] ?? Enumerable.Empty<JToken>())
-                        {
-                            parameters.Add(query["name"].Value<string>(), query["value"].Value<string>());
-                        }
-                        foreach (var param in entry["request"]["postData"]?["params"] ?? Enumerable.Empty<JToken>())
-                        {
-                            parameters.Add(param["name"].Value<string>(), param["value"].Value<string>());
-                        }
-                        requests.Add(new ParsedRequest
-                        {
-                            Url = entry["request"]["url"].ToString(),
-                            Method = entry["request"]["method"].ToString(),
-                            ContentType = entry["request"]["postData"]?["mimeType"]?.ToString() ?? "",
-                            Headers = new JObject(entry["request"]["headers"].Select(h => new JProperty(h["name"].Value<string>().ToLower(), h["value"])).Concat(entry["request"]["postData"]?["params"]?.Select(p => new JProperty(p["name"].Value<string>(), p["value"].Value<string>())) ?? Enumerable.Empty<JProperty>())),
-                            Params = parameters,
-                            Body = entry["request"]["postData"]?["text"]?.ToString() ?? "",
-                            Response = new ParsedResponse
-                            {
-                                Status = entry["response"]["status"].ToString(),
-                                ContentType = entry["response"]["content"]["mimeType"].ToString(),
-                                Headers = new JObject(entry["response"]["headers"].Select(h => new JProperty(h["name"].Value<string>().ToLower(), h["value"]))),
-                                Body = entry["response"]["content"]?["text"]?.ToString() ?? ""
-                            }
-                        });
+                        ParsedRequest request = new(entry["request"] as JObject, entry["response"] as JObject);
+                        request.SetTime(entry["startedDateTime"].Value<string>(), entry["time"].Value<double>());
+                        requests.Add(request);
                     }
                     foreach (var request in requests)
                     {
@@ -72,11 +53,10 @@ namespace UserReplay
                     session.Authenticated = new AuthInfo
                     {
                         Type = authType,
-                        Credentials = authHeader?.Headers["authorization"] ?? JValue.CreateNull()
+                        Credentials = authHeader?.Headers["authorization"] ?? ""
                     };
 
                     Log.Information(session.ToString());
-
 
                 }
                 return Task.FromResult(true);
@@ -91,30 +71,114 @@ namespace UserReplay
         public class ParsedRequest
         {
             public string Url { get; set; }
-            public string Method { get; set; }
-            public string ContentType { get; set; }
-            public JObject Headers { get; set; }
+            public HttpMethod Method { get; set; }
+            public Dictionary<string, string> QueryParams { get; set; }
+            public Dictionary<string, string> Headers { get; set; }
             public string Body { get; set; }
-            public JObject Params { get; set; }
             public ParsedResponse Response { get; set; }
+            public DateTime StartTime { get; set; }
+            public TimeSpan CallDuration { get; set; }
+            public string RequestVersion { get; set; }
+
+            public ParsedRequest(JObject request, JObject response)
+            {
+                Url = request["url"].Value<string>();
+                Method = Enum.Parse<HttpMethod>(request["method"].Value<string>());
+                Headers = request.ContainsKey("headers") ? (request["headers"] as JArray).ToDictionary(h => h["name"].Value<string>(), h => h["value"].Value<string>()) : new Dictionary<string, string>();
+                QueryParams = request.ContainsKey("queryString") ? (request["queryString"] as JArray).ToDictionary(q => q["name"].Value<string>(), q => q["value"].Value<string>()) : new Dictionary<string, string>();
+                Response = new ParsedResponse(response);
+                Body = request.ContainsKey("postData") ? request["postData"]["text"].Value<string>() : "";
+                RequestVersion = request["httpVersion"].Value<string>();
+            }
+
+            public void SetTime(string startTimeString, double callDuration)
+            {
+                StartTime = DateTime.Parse(startTimeString, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind);
+                CallDuration = TimeSpan.FromMilliseconds(callDuration);
+            }
+
+            internal static readonly string[] supportedHttpVersions = ["1.0", "1.1", "2.0", "3.0"];
+            public async Task<IFlurlResponse> Replay()
+            {
+                IFlurlRequest request = new FlurlRequest(Url).AllowAnyHttpStatus();
+                string[] splitHttpVersion = RequestVersion.Split("/");
+                if (splitHttpVersion.Length > 1 && supportedHttpVersions.Contains(splitHttpVersion[1]))
+                {
+                    request.WithSettings(s => s.HttpVersion = splitHttpVersion[1]);
+                }
+                else
+                {
+                    request.WithSettings(s => s.HttpVersion = "1.1");
+                }
+                foreach (var header in Headers)
+                {
+                    // skip headers that are not allowed with Flurl
+                    if (header.Key.StartsWith(":") || header.Key.Equals("content-length", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        request.WithHeader(header.Key, header.Value);
+                    }
+                }
+                foreach (var query in QueryParams)
+                {
+                    request.SetQueryParam(query.Key, query.Value);
+                }
+                return Method switch
+                {
+                    HttpMethod.GET => await request.GetAsync(),
+                    HttpMethod.POST => await request.PostStringAsync(Body),
+                    HttpMethod.PUT => await request.PutStringAsync(Body),
+                    HttpMethod.PATCH => await request.PatchStringAsync(Body),
+                    HttpMethod.OPTIONS => await request.OptionsAsync(),
+                    HttpMethod.DELETE => await request.DeleteAsync(),
+                    _ => throw new InvalidOperationException()
+                };
+            }
 
             public override string ToString()
             {
-                return $"{Method} {Url} - {Params}{(Method != "GET" && !string.IsNullOrEmpty(Body) ? $"\nBody: {Body}" : "")} \n==>\n{Response}\n";
+                return $"REQUEST {Method} {Url} - {JObject.FromObject(QueryParams)}{(Method != HttpMethod.GET && !string.IsNullOrEmpty(Body) ? $"\nBody: {Body}" : "")} \n==>\n{Response}\n";
             }
         }
 
 
         public class ParsedResponse
         {
-            public string Status { get; set; }
-            public string ContentType { get; set; }
-            public JObject Headers { get; set; }
+            public int Status { get; set; }
+            public Dictionary<string, string> Headers { get; set; }
             public string Body { get; set; }
 
+            public ParsedResponse(JObject response)
+            {
+                Status = response["status"].Value<int>();
+                Headers = response.ContainsKey("headers") ? (response["headers"] as JArray).ToDictionary(h => h["name"].Value<string>(), h => h["value"].Value<string>()) : new Dictionary<string, string>();
+                Body = response["content"]["text"].Value<string>();
+            }
+            public ParsedResponse(IFlurlResponse response)
+            {
+                Status = response.StatusCode;
+                Headers = response.Headers.ToDictionary(h => h.Name, h => h.Value);
+                Body = response.ResponseMessage.Content.ReadAsStringAsync().Result;
+            }
             public override string ToString()
             {
-                return $"RESPONSE {Status} : ({ContentType}){(!string.IsNullOrEmpty(Body) ? $"\nBody: {(Body.Length > 200 ? Body[..200] + "......" : Body)}" : "")}\n";
+                return $"RESPONSE {Status} : {(!string.IsNullOrEmpty(Body) ? $"\nBody: {(Body.Length > 200 ? Body[..200] + "......" : Body)}" : "")}\n";
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is ParsedResponse response &&
+                       Status == response.Status &&
+                        EqualityComparer<Dictionary<string, string>>.Default.Equals(Headers, response.Headers) &&
+                        Body.Trim() == response.Body.Trim();
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(Status, Headers, Body);
             }
         }
 
@@ -145,6 +209,22 @@ namespace UserReplay
             Basic,
             Bearer,
             Other
+        }
+
+        public enum HttpMethod
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE,
+            PATCH,
+            OPTIONS,
+            HEAD,
+            /*
+            not needed for now
+            CONNECT,
+            TRACE
+            */
         }
 
     }
--- UserReplay/Replay.cs
+++ UserReplay/Replay.cs
@@ -4,6 +4,8 @@ using Flurl.Http;
 using Newtonsoft.Json.Linq;
 using Serilog;
 using Flurl;
+using System.Net;
+using static UserReplay.Parse;
 
 namespace UserReplay
 {
@@ -27,47 +29,22 @@ namespace UserReplay
                     // naive implementation
                     foreach (var entry in har["log"]["entries"])
                     {
-                        IFlurlRequest request = new FlurlRequest(entry["request"]["url"].Value<string>()).AllowAnyHttpStatus();
-                        foreach (var header in entry["request"]["headers"])
+                        ParsedRequest request = new(entry["request"] as JObject, entry["response"] as JObject);
+                        IFlurlResponse response = await request.Replay();
+                        ParsedResponse parsedResponse = new(response);
+                        Log.Information(request.ToString());
+                        Log.Information(parsedResponse.ToString());
+                        if (parsedResponse != request.Response)
                         {
-                            // skip headers that are not allowed with Flurl
-                            if (header["name"].Value<string>().StartsWith(":") || header["name"].Value<string>().Equals("content-length", StringComparison.CurrentCultureIgnoreCase))
+                            if (parsedResponse.Status >= 400)
                             {
-                                continue;
+                                Log.Error($"Request failed unexpectedly: {parsedResponse.Status}");
+                                return false;
                             }
                             else
                             {
-                                string headerName = header["name"].Value<string>();
-                                string headerValue = header["value"].Value<string>();
-                                request.WithHeader(headerName, headerValue);
+                                Log.Warning($"Request was successful but response did not match recorded response");
                             }
-                        }
-                        foreach (var query in entry["request"]["queryString"])
-                        {
-                            string queryName = query["name"].Value<string>();
-                            string queryValue = query["value"].Value<string>();
-                            request.SetQueryParam(queryName, queryValue);
-                        }
-                        Log.Information($"Sending {entry["request"]["method"]} request to {entry["request"]["url"]} - expecting {entry["response"]["status"]} response");
-                        //return true;
-                        var response = entry["request"]["method"].Value<string>() switch
-                        {
-                            "GET" => await request.GetAsync(),
-                            "POST" => await request.PostStringAsync(entry["request"]["postData"]["text"].ToString()),
-                            "PUT" => await request.PutStringAsync(entry["request"]["postData"]["text"].ToString()),
-                            "PATCH" => await request.PatchStringAsync(entry["request"]["postData"]["text"].ToString()),
-                            "OPTIONS" => await request.OptionsAsync(),
-                            "DELETE" => await request.DeleteAsync(),
-                            _ => throw new InvalidOperationException()
-                        };
-                        if (response.StatusCode >= 400)
-                        {
-                            Log.Error($"Endpoint responded {response.StatusCode} (original status: {entry["response"]["status"]})");
-                            return false;
-                        }
-                        else
-                        {
-                            Log.Information($"Endpoint responded {response.StatusCode} {response.ResponseMessage} (original status: {entry["response"]["status"]})");
                         }
                     }
                     return true;
