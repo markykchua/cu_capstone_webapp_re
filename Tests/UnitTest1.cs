94f1d890bf48f9ecc22e604ae73356e6756a3037
Quinton Tracey <qistoocool@gmail.com>
2024-10-31 17:45:23 -0400

    Initial Test Commit

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using static UserReplay.Parse;
+namespace Tests;
+public class Tests
+{
+    private UserReplay.Session _session; 
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
